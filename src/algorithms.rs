8a5cf4514e76c32b8c0933e00842a5e0c164e924
Daniel P. Clark <6ftdan@gmail.com>
2018-09-18 20:57:09 -0400

    WIP

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -33,24 +33,24 @@ pub(crate) fn put_u32(state: u32, data: &mut [u8], index: usize) {
 #[inline(always)] pub(crate) fn rotr8(x: u32) -> u32 {  ( ( x.wrapping_shl(24) ) & 0xFFFFFFFF )
                                               | ( ( x & 0xFFFFFFFF ).wrapping_shr(8) ) }
 
-// #define           SHR(x,      n     )          ((x & 0xFFFFFFFF) >> n)
-#[inline(always)] fn shr(x: u32, n: u32)  -> u32 { (x & 0xFFFFFFFF).wrapping_shr(n)         }
-// #define           ROTR(x,      n     )         (SHR(x,n) | (x << (32 - n)))
+// SHRⁿ(x) = x >> n
+#[inline(always)] fn shr(x: u32, n: u32)  -> u32 { (x).wrapping_shr(n)         }
+// ROTRⁿ(x) = (x >> n) ∨ (x << w - n)
 #[inline(always)] fn rotr(x: u32, n: u32) -> u32 { shr(x,n) | (x.wrapping_shl(32 - n))    }
 
-// #define           S0(x     )         (ROTR(x, 7) ^ ROTR(x,18) ^  SHR(x, 3))
-#[inline(always)] fn s0(x: u32) -> u32 { rotr(x, 7) ^ rotr(x,18) ^  shr(x, 3)    }
-// #define           S1(x     )         (ROTR(x,17) ^ ROTR(x,19) ^  SHR(x,10))
-#[inline(always)] fn s1(x: u32) -> u32 { rotr(x,17) ^ rotr(x,19) ^  shr(x,10)    }
-// #define           S2(x     )         (ROTR(x, 2) ^ ROTR(x,13) ^ ROTR(x,22))
-#[inline(always)] fn s2(x: u32) -> u32 { rotr(x, 2) ^ rotr(x,13) ^ rotr(x,22)    }
-// #define           S3(x     )         (ROTR(x, 6) ^ ROTR(x,11) ^ ROTR(x,25))
-#[inline(always)] fn s3(x: u32) -> u32 { rotr(x, 6) ^ rotr(x,11) ^ rotr(x,25)    }
+// Ch( x, y, z) = ( x ^ y) ⊕  ( x ^ z)
+#[inline(always)] fn ch(x: u32, y: u32, z: u32) -> u32 { (x & y) ^ (x & z) }
+// Maj( x, y, z) = ( x ^ y) ⊕ ( x ^ z) ⊕ ( y ^ z)
+#[inline(always)] fn maj(x: u32, y: u32, z: u32) -> u32 { ch(x, y, z) ^ (y & z) }
 
-// #define           F0(x,      y,      z     )         ((x & y) | (z & (x | y)))
-#[inline(always)] fn f0(x: u32, y: u32, z: u32) -> u32 { (x & y) | (z & (x | y)) }
-// #define           F1(x,      y,      z     )         (z ^ (x & (y ^ z)))
-#[inline(always)] fn f1(x: u32, y: u32, z: u32) -> u32 { z ^ (x & (y ^ z))       }
+// σ{256}0(x) = ROTR⁷(x) ⊕ ROTR¹⁸(x) ⊕ ROTR³(x)
+#[inline(always)] fn s0(x: u32) -> u32 { rotr(x, 7) ^ rotr(x,18) ^  shr(x, 3)    }
+// σ{256}1(x) = ROTR¹⁷(x) ⊕ ROTR¹⁹(x) ⊕ ROTR¹⁰(x)
+#[inline(always)] fn s1(x: u32) -> u32 { rotr(x,17) ^ rotr(x,19) ^  shr(x,10)    }
+// Σ{256}0(x) = ROTR²(x) ⊕ ROTR¹³(x) ⊕ ROTR²²(x)
+#[inline(always)] fn s2(x: u32) -> u32 { rotr(x, 2) ^ rotr(x,13) ^ rotr(x,22)    }
+// Σ{256}1(x) = ROTR⁶(x) ⊕ ROTR¹¹(x) ⊕ ROTR²⁵(x)
+#[inline(always)] fn s3(x: u32) -> u32 { rotr(x, 6) ^ rotr(x,11) ^ rotr(x,25)    }
 
 #[inline(always)] pub(crate) fn r(w: &mut [u32; 64], t: usize) -> u32 {
     w[t] = s1(w[t -  2]).wrapping_add(w[t -  7]).wrapping_add(s0(w[t - 15])).wrapping_add(w[t - 16]);
@@ -58,24 +58,20 @@ pub(crate) fn put_u32(state: u32, data: &mut [u8], index: usize) {
 }
 
 #[inline(always)]
-pub(crate) fn p(a: u32, b: u32, c: u32, d: &mut u32, e: u32, f: u32, g: u32, h: &mut u32, x: u32, k: u32) {
-    let temp1 = (*h).wrapping_add(s3(e)).wrapping_add(f1(e,f,g)).wrapping_add(k).wrapping_add(x);
-    let temp2 = s2(a).wrapping_add(f0(a,b,c));
-    *d = (*d).wrapping_add(temp1);
-    *h = temp1.wrapping_add(temp2);
+pub(crate) fn p(
+    a: &mut u32,    b: &mut u32,
+    c: &mut u32,    d: &mut u32,
+    e: &mut u32,    f: &mut u32,
+    g: &mut u32,    h: &mut u32,
+    x: u32,         k: u32) {
+    let temp1 = (*h).wrapping_add(s3(*e)).wrapping_add(ch(*e,*f,*g)).wrapping_add(k).wrapping_add(x);
+    let temp2 = s2(*a).wrapping_add(maj(*a,*b,*c));
+    *h = *g;
+    *g = *f;
+    *f = *e;
+    *e = (*d).wrapping_add(temp1);
+    *d = *c;
+    *c = *b;
+    *b = *a;
+    *a = temp1.wrapping_add(temp2);
 }
-
-// #[inline(always)]
-// pub(crate) fn p(a: u32, b: u32, c: u32, d: &mut u32, e: u32, f: u32, g: u32, h: &mut u32, x: u32, k: u32) {
-//     let temp1 = (*h as u64) + s3(e) as u64 + f1(e,f,g) as u64 + k as u64 + x as u64;
-//     let temp2 = s2(a) as u64 + f0(a,b,c) as u64;
-// 
-//     if temp1 > u32::max_value() as u64 { panic!("u32 exceeded! algorithms::p variable temp1 {:X} > {:X}", temp1, u32::max_value()); }
-//     if temp2 > u32::max_value() as u64 { panic!("u32 exceeded! algorithms::p variable temp2 {:X} > {:X}", temp2, u32::max_value()); }
-// 
-//     let temp3 = temp1 + temp2;
-//     if temp3 > u32::max_value() as u64 { panic!("u32 exceeded! algorithms::p variable temp3 {:X} > {:X}", temp3, u32::max_value()); }
-// 
-//     *d += temp1 as u32;
-//     *h = temp3 as u32;
-// }
